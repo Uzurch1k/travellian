75efee1d7a283f26a60f7b439a252e5a0c64d81e
Uzurch1k <galcenkovlad7@gmail.com>
2024-02-22 02:40:15 +0200

    fix now

---
+++ src/main.js
@@ -0,0 +1,38 @@
+// ==============================================================
+
+const refs = {
+  body: document.querySelector('body'),
+  burger: document.querySelector('.header-burger'),
+  headerNav: document.querySelector('.header-nav'),
+  headerLink: document.querySelectorAll('.header-item'),
+};
+
+// ========================= Bugrer =========================
+
+refs.burger.addEventListener('click', () => {
+  refs.burger.classList.toggle('active');
+  refs.headerNav.classList.toggle('active');
+  refs.body.classList.toggle('lock');
+});
+
+const mediaQueryList = window.matchMedia('(max-width: 1151px)');
+
+mediaQueryList.addEventListener('change', event => {
+  if (event.matches) {
+    refs.headerLink.forEach(headerLink => {
+      headerLink.addEventListener('click', () => {
+        refs.burger.classList.toggle('active');
+        refs.headerNav.classList.toggle('active');
+        refs.body.classList.toggle('lock');
+      });
+    });
+  }
+});
+
+// ==============================================================
+
+// ==============================================================
+
+// ==============================================================
+
+// ==============================================================
